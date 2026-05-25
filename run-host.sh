be3681d5653eb16b3a9d1e080f17f92ed040c3cd
reyzzzl <ahmadrizkiafrizal1@gmail.com>
2026-05-26 04:20:25 +0700

    Update run-host.sh

--- run-host.sh
+++ run-host.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-docker run -it --rm --name hyperion-host -p 9999:9999 -v ~/.hyperion:/root/.hyperion hyperion:latest host
+echo "Starting Hyperion in HOST mode..."
+echo ""
+docker run -it \
+  --rm \
+  --name hyperion-host \
+  -p 9999:9999 \
+  -v ~/.hyperion:/root/.hyperion \
+  hyperion:latest host
