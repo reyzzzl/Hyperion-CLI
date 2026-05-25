b14de5e0a7d74735a532e157229e2cc5c4cf8424
reyzzzl <ahmadrizkiafrizal1@gmail.com>
2026-05-26 04:27:24 +0700

    Update run-client.sh

--- run-client.sh
+++ run-client.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-read -p "Enter peer address: " addr
-docker run -it --rm -v ~/.hyperion:/root/.hyperion hyperion:latest connect $addr
+echo "Enter peer address (e.g., localhost:9999 or xyz.onion):"
+read PEER_ADDR
+echo ""
+echo "Connecting to $PEER_ADDR..."
+docker run -it \
+  --rm \
+  -v ~/.hyperion:/root/.hyperion \
+  hyperion:latest connect $PEER_ADDR
