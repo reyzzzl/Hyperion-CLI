92e138109fdc4e918dbf2b62a398f933c2992290
reyzzzl <ahmadrizkiafrizal1@gmail.com>
2026-05-26 04:41:29 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ setup(
     name="hyperion-pqc",
     version="2.0.0",
     packages=find_packages(),
+    include_package_data=True,
+    python_requires=">=3.8",
     install_requires=[
         'liboqs-python>=0.8.0',
         'cryptography>=41.0.0',
@@ -15,5 +17,25 @@ setup(
         'console_scripts': [
             'hyperion=hyperion.cli.commands:main'
         ]
-    }
+    },
+    author="Hyperion Team",
+    author_email="hyperionteam@proton.me",
+    description="Post-Quantum Secure Messenger with Kyber-512, Double Ratchet, and Tor",
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/reyzzzl/Hyperion-CLI",
+    license="GPL-3.0",
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Topic :: Security :: Cryptography",
+        "Topic :: Communications :: Chat",
+        "Operating System :: OS Independent",
+    ],
+    keywords="post-quantum cryptography, kyber, dilithium, tor, e2ee, p2p, messenger",
 )
