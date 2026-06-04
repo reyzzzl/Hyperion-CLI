82e62e4acc4accc49c804792a456fd65fa0bd2eb
reyzzzl <ahmadrizkiafrizal1@gmail.com>
2026-06-04 14:17:27 +0700

    Update logic kdf

--- hyperion_core/src/kdf.rs
+++ hyperion_core/src/kdf.rs
@@ -1,46 +1,24 @@
 // SPDX-License-Identifier: GPL-3.0
-use blake2::{Blake2b512, Digest};
+use argon2::{Argon2, Params, Algorithm, Version};
+use hkdf::Hkdf;
 use sha2::Sha512;
-use hmac::{Hmac, Mac};
 use zeroize::Zeroizing;
 
 pub const SALT_SIZE: usize = 32;
 pub const KEY_LEN: usize = 32;
 
+// downing the for pw to argon2id
 pub fn derive_key(pwd: &[u8], salt: &[u8]) -> Zeroizing<[u8; KEY_LEN]> {
-    let mut hasher = Blake2b512::new();
-    hasher.update(salt);
-    hasher.update(pwd);
-    let result = hasher.finalize();
     let mut out = Zeroizing::new([0u8; KEY_LEN]);
-    out.copy_from_slice(&result[..KEY_LEN]);
+    let argon2 = Argon2::new(Algorithm::Argon2id, Version::V0x13, Params::default());
+    argon2.hash_password_into(pwd, salt, &mut *out).expect("argon2id error");
     out
 }
 
-pub fn hkdf(salt: &[u8], ikm: &[u8], info: &[u8], length: usize) -> Vec<u8> {
-    type HmacSha512 = Hmac<Sha512>;
-    let prk = if salt.is_empty() {
-        let mut mac = HmacSha512::new_from_slice(&[0u8; 64]).unwrap();
-        mac.update(ikm);
-        mac.finalize().into_bytes().to_vec()
-    } else {
-        let mut mac = HmacSha512::new_from_slice(salt).unwrap();
-        mac.update(ikm);
-        mac.finalize().into_bytes().to_vec()
-    };
-    let mut out = Vec::with_capacity(length);
-    let mut t = vec![];
-    let mut i = 1u8;
-    while out.len() < length {
-        let mut mac = HmacSha512::new_from_slice(&prk).unwrap();
-        mac.update(&t);
-        mac.update(info);
-        mac.update(&[i]);
-        let block = mac.finalize().into_bytes();
-        t = block.to_vec();
-        out.extend_from_slice(&t);
-        i += 1;
-    }
-    out.truncate(length);
+// hdkf-sha512 for salt this optional
+pub fn hkdf(salt: Option<&[u8]>, ikm: &[u8], info: &[u8], length: usize) -> Vec<u8> {
+    let hk = Hkdf::<Sha512>::new(salt, ikm);
+    let mut out = vec![0u8; length];
+    hk.expand(info, &mut out).unwrap(); // limited length
     out
 }
