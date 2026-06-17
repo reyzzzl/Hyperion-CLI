91cc09d617116a7c5a96f6f333421e843b9dab5e
reyzzzl <ahmadrizkiafrizal1@gmail.com>
2026-06-17 14:48:46 +0700

    Delete run-client.sh

--- run-client.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Enter peer address (e.g., localhost:9999 or xyz.onion):"
-read PEER_ADDR
-echo ""
-echo "Connecting to $PEER_ADDR..."
-docker run -it \
-  --rm \
-  -v ~/.hyperion:/root/.hyperion \
-  hyperion:latest connect "$PEER_ADDR"
