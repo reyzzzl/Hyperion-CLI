d60d81a4c03052f7499891f6f3e95c456a449f57
reyzzzl <ahmadrizkiafrizal1@gmail.com>
2026-06-25 21:06:47 +0700

    Update kyber.rs

--- src/crypto/kyber.rs
+++ src/crypto/kyber.rs
@@ -1,33 +1,31 @@
-// SPDX-License-Identifier: GPL-3.0
-use oqs::kem::Kem;
-use zeroize::Zeroize;
+use oqs::kem::{Kem, Algorithm};
+use zeroize::{ZeroizeOnDrop, Zeroizing};
 use serde::{Serialize, Deserialize};
 use serde_bytes;
 use anyhow::{Result, anyhow};
-
-#[derive(Serialize, Deserialize)]
+// TODO: i wanna using cache kem instance next avoid repeated allocation
+// options is: lazy_static/once cell,
+// accept &kem parameter
+#[derive(Serialize, Deserialize, ZeroizeOnDrop)]
 pub struct SecretKey(#[serde(with = "serde_bytes")] pub Vec<u8>);
 
-impl Drop for SecretKey {
-    fn drop(&mut self) {
-        self.0.zeroize();
-    }
-}
-
 pub fn keypair() -> Result<(Vec<u8>, SecretKey)> {
-    let kem = Kem::new("Kyber1024").map_err(|e| anyhow!(e))?;
-    let (pk, sk) = kem.keypair().map_err(|e| anyhow!(e))?;
-    Ok((pk, SecretKey(sk)))
+    let kem = Kem::new(Algorithm::Kyber1024).map_err(|e| anyhow!("{:?}", e))?;
+    let (pk, sk) = kem.keypair().map_err(|e| anyhow!("{:?}", e))?;
+    Ok((pk.into_vec(), SecretKey(sk.into_vec())))
 }
 
-pub fn encaps(pk: &[u8]) -> Result<(Vec<u8>, Vec<u8>)> {
-    let kem = Kem::new("Kyber1024").map_err(|e| anyhow!(e))?;
-    let (ct, ss) = kem.encapsulate(pk).map_err(|e| anyhow!(e))?;
-    Ok((ct, ss))
+pub fn encaps(pk: &[u8]) -> Result<(Vec<u8>, Zeroizing<Vec<u8>>)> {
+    let kem = Kem::new(Algorithm::Kyber1024).map_err(|e| anyhow!("{:?}", e))?;
+    let pk_obj = kem.public_key_from_bytes(pk).map_err(|e| anyhow!("{:?}", e))?;
+    let (ct, ss) = kem.encapsulate(&pk_obj).map_err(|e| anyhow!("{:?}", e))?;
+    Ok((ct.into_vec(), Zeroizing::new(ss.into_vec())))
 }
 
-pub fn decaps(sk: &SecretKey, ct: &[u8]) -> Result<Vec<u8>> {
-    let kem = Kem::new("Kyber1024").map_err(|e| anyhow!(e))?;
-    let ss = kem.decapsulate(ct, &sk.0).map_err(|e| anyhow!(e))?;
-    Ok(ss)
+pub fn decaps(sk: &SecretKey, ct: &[u8]) -> Result<Zeroizing<Vec<u8>>> {
+    let kem = Kem::new(Algorithm::Kyber1024).map_err(|e| anyhow!("{:?}", e))?;
+    let ct_obj = kem.ciphertext_from_bytes(ct).map_err(|e| anyhow!("{:?}", e))?;
+    let sk_obj = kem.secret_key_from_bytes(&sk.0).map_err(|e| anyhow!("{:?}", e))?;
+    let ss = kem.decapsulate(&sk_obj, &ct_obj).map_err(|e| anyhow!("{:?}", e))?;
+    Ok(Zeroizing::new(ss.into_vec()))
 }
