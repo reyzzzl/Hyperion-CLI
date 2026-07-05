9765d3a64fdc182f2e482e60d248642be7283fc3
reyzzzl <ahmadrizkiafrizal1@gmail.com>
2026-07-05 22:03:41 +0700

    Update kyber.rs, fix error

--- src/crypto/kyber.rs
+++ src/crypto/kyber.rs
@@ -15,15 +15,15 @@ pub fn keypair() -> Result<(Vec<u8>, SecretKey)> {
 
 pub fn encaps(pk: &[u8]) -> Result<(Vec<u8>, Zeroizing<Vec<u8>>)> {
     let kem = Kem::new(Algorithm::Kyber1024).map_err(|e| anyhow!("{:?}", e))?;
-    let pk_obj = kem.public_key_from_bytes(pk).map_err(|e| anyhow!("{:?}", e))?;
+    let pk_obj = kem.public_key_from_bytes(pk).ok_or_else(|| anyhow!("invalid public key"))?;
     let (ct, ss) = kem.encapsulate(&pk_obj).map_err(|e| anyhow!("{:?}", e))?;
     Ok((ct.into_vec(), Zeroizing::new(ss.into_vec())))
 }
 
 pub fn decaps(sk: &SecretKey, ct: &[u8]) -> Result<Zeroizing<Vec<u8>>> {
     let kem = Kem::new(Algorithm::Kyber1024).map_err(|e| anyhow!("{:?}", e))?;
-    let ct_obj = kem.ciphertext_from_bytes(ct).map_err(|e| anyhow!("{:?}", e))?;
-    let sk_obj = kem.secret_key_from_bytes(&sk.0).map_err(|e| anyhow!("{:?}", e))?;
+    let ct_obj = kem.ciphertext_from_bytes(ct).ok_or_else(|| anyhow!("invalid ciphertext"))?;
+    let sk_obj = kem.secret_key_from_bytes(&sk.0).ok_or_else(|| anyhow!("invalid secret key"))?;
     let ss = kem.decapsulate(&sk_obj, &ct_obj).map_err(|e| anyhow!("{:?}", e))?;
     Ok(Zeroizing::new(ss.into_vec()))
 }
