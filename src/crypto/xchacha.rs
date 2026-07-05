4bcb53baeae282e98bde6bc128179085aa86f770
reyzzzl <ahmadrizkiafrizal1@gmail.com>
2026-07-05 22:32:19 +0700

    Update xchacha.rs

--- src/crypto/xchacha.rs
+++ src/crypto/xchacha.rs
@@ -1,17 +1,26 @@
-use chacha20poly1305::{XChaCha20Poly1305, Key, Nonce};
-// fix previous import in XChaCha20Poly1305 
-use anyhow::Result;
+use chacha20poly1305::{XChaCha20Poly1305, Key, XNonce};
+use chacha20poly1305::aead::{Aead, KeyInit, Payload};
+use rand::RngCore;
+use anyhow::{Result, anyhow};
 
-pub fn encrypt(key: &[u8; 32], nonce: &[u8; 24], data: &[u8]) -> Result<Vec<u8>> {
+pub fn encrypt(key: &[u8; 32], plaintext: &[u8], aad: &[u8]) -> Result<(Vec<u8>, Vec<u8>)> {
     let cipher = XChaCha20Poly1305::new(Key::from_slice(key));
-    let nonce = Nonce::from_slice(nonce);
-    cipher.encrypt(nonce, data).map_err(Into::into)
+    let mut nonce_bytes = [0u8; 24];
+    rand::rngs::OsRng.fill_bytes(&mut nonce_bytes);
+    let nonce = XNonce::from_slice(&nonce_bytes);
+    let ct = cipher
+        .encrypt(nonce, Payload { msg: plaintext, aad })
+        .map_err(|e| anyhow!("encrypt failed: {:?}", e))?;
+    Ok((nonce_bytes.to_vec(), ct))
 }
 
-pub fn decrypt(key: &[u8; 32], nonce: &[u8; 24], data: &[u8]) -> Result<Vec<u8>> {
+pub fn decrypt(key: &[u8; 32], nonce: &[u8], ciphertext: &[u8], aad: &[u8]) -> Result<Vec<u8>> {
     let cipher = XChaCha20Poly1305::new(Key::from_slice(key));
-    let nonce = Nonce::from_slice(nonce);
-    cipher.decrypt(nonce, data).map_err(Into::into)
+    let nonce = XNonce::from_slice(nonce);
+    cipher
+        .decrypt(nonce, Payload { msg: ciphertext, aad })
+        .map_err(|e| anyhow!("decrypt failed: {:?}", e))
 }
-// TODO: use ZeroizeOnDrop or reference key is  clean after use 
-// tbh i want to use, ZeroizeOnDrop in dilithium. is minimally security bt good enough for just hackclub demo 
+// TODO: ZeroizeOnDrop integration to key really clean when dropped
+// tbh for a hacklub demo this is enough 
+// mybe next i wanna setup refactor for profer
